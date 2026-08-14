49ecac3b38fe208fbead6b70a4892678b647680b
Shlomi Ganon <Shlomi1395@Gmail.com>
2026-08-14 17:20:32 +0300

    fix collectable disappear on trigger

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -13,12 +13,15 @@ public class Collectable : MonoBehaviour
     [SerializeField] private ScoreManager scoreManager;
     [SerializeField] private CollectableType collectableType;
 
-    
-    private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            scoreManager.CountCollectables(this.collectableType);
+            if (scoreManager)
+            {
+                scoreManager.CountCollectables(this.collectableType);
+            }
             Destroy(gameObject);
         }
     }
