aaf6879e9dc6c4329a461ce14fc98229954f015c
Shlomi Ganon <Shlomi1395@Gmail.com>
2026-08-14 16:16:25 +0300

    write all scripts (movement , controller) for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,29 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private PlayerMovement pm;
     void Start()
     {
-        
+        pm = GetComponent<PlayerMovement>();
+        if (!pm) Debug.LogError("can't find PlayerMovement script!");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMove(InputValue value)
     {
-        
+        float XValue = value.Get<Vector2>().x;
+        pm.HandleHorizontalMove(XValue);
     }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            pm.HandleJump();
+        }
+    }
+
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Vector2 moveDirections;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 7f;
+    bool isGrounded = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        if (!rb) Debug.LogError("the script can't find any Rigidbody2D!");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HandleHorizontalMove(float HorizontalDirection)
     {
-        
+        this.moveDirections = new Vector2(HorizontalDirection, 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    public void HandleJump()
+    {
+        if (this.isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveDirections.x * moveSpeed, rb.linearVelocity.y);
     }
 }
