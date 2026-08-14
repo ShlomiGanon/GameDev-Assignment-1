039fe239ca99a84eb253478b6b0ba345550f7021
Shlomi Ganon <Shlomi1395@Gmail.com>
2026-08-14 17:22:35 +0300

    modifie collectable script

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -13,7 +13,6 @@ public class Collectable : MonoBehaviour
     [SerializeField] private ScoreManager scoreManager;
     [SerializeField] private CollectableType collectableType;
 
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
