7e83343e91483c7cc0a334d132989300c3871c72
Maor Buchbut <hellz35u@gmail.com>
2026-08-15 14:37:24 +0300

    Bug fixed

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,28 +2,37 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    [SerializeField] private Transform platform;
     [SerializeField] private Transform topPoint;
-    [SerializeField] private Transform BottomPoint;
+    [SerializeField] private Transform bottomPoint;
     [SerializeField] private float speed = 2f;
 
+    private Rigidbody2D rb;
     private Transform target;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
     {
         target = topPoint;
     }
 
     private void FixedUpdate()
     {
-        platform.position = Vector3.MoveTowards(
-            platform.position,
+        Vector2 newPosition = Vector2.MoveTowards(
+            rb.position,
             target.position,
-            speed * Time.deltaTime
-            );
-        if (Vector3.Distance(platform.position, target.position ) < 0.01f)
+            speed * Time.fixedDeltaTime
+        );
+
+        rb.MovePosition(newPosition);
+
+        if (Vector2.Distance(rb.position, target.position) < 0.01f)
         {
-            target = target == topPoint ? BottomPoint : topPoint;
+            target = target == topPoint ? bottomPoint : topPoint;
         }
     }
+
 }
